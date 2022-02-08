9e6924f68d78a186de36c7faa0898860b91f6b9d
akramjerbi91 <akram239911@gmail.com>
2022-02-08 18:17:39 +0100

    [200~a function that computes the absolute value of an integer.~

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_sign - a function that computes the absolute value of an integer.
+ *
+ * Return: Always 0.
+ */
+
+ int _abs(int x)
+
+ {
+
+x = x * -1;
+return (x);
+
+ }
