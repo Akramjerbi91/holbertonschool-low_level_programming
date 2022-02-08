8168d344075d1567186cc251bee2877673821773
akramjerbi91 <akram239911@gmail.com>
2022-02-08 17:29:26 +0100

    [200~a function that prints the sign of a number.~

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,20 +11,20 @@
 int print_sign(int n)
 {
 if (n > 0)
-{
+
 	putchar('+');
 	return (1);
-}
+
 	else if (n < 0)
-{
+
 	putchar('-');
 	return (-1);
-}
+
 	else
-}
+
 	putchar('0');
 	return (0);
 
 }
 
-}
+
