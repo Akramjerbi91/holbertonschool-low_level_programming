f3ca9202d851350506b6d17c412ab34cfc76de1c
akramjerbi91 <akram239911@gmail.com>
2022-02-09 15:54:29 +0100

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,19 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	while (n != 98)
 {
-	if (i < 98)
+	if (n < 98)
 {
-	printf("%d, ", i);
+	n++;
+	printf("%d, ", n);
 }
-	else
+	else if (n > 98)
 {
-	printf("98\n");
-}
-}
-	for (i = n; i >= 98; i--)
-{
-	if (i > 98)
-{
-	printf("%d, ", i);
+	n--;
+	printf("%d, ", n);
 }
 }
+	if (n == 98)
+	printf("%d\n", n);
 }
