105d7ae864612b51a76c112d3002fed291c52a7f
akramjerbi91 <akram239911@gmail.com>
2022-02-10 17:59:14 +0100

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
 printf("FizzBuzz ");
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i != 100)
 printf("Buzz ");
 else if (i % 3 == 0)
 printf("Fizz ");
 else if (i == 100)
-printf("%d\n", i);
+printf("Buzz\n");
 else
 printf("%d ", i);
 }
