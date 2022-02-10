5cdbbd1c44700891652e31cfd7c89a744b97d3e6
akramjerbi91 <akram239911@gmail.com>
2022-02-10 13:01:10 +0100

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@
 
 void more_numbers(void)
 {
-	int i, j = 0;
+	int i, j;
 
-while (j < 10)
+	for (j = 0; j < 10; j++)
 {
 	for (i = 0; i <= 14; i++)
 	{
@@ -21,8 +21,7 @@ while (j < 10)
 }
 		_putchar((i % 10) + 48);
 		}
+		_putchar(10);
 }
-	_putchar(10);
-	j++;
-}
+
 }
