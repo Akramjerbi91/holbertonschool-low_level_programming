85ba08e8737cb8f3c1be94dbbfe23e5c6ae196db
akramjerbi91 <akram239911@gmail.com>
2022-02-11 13:32:14 +0100

    task11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,34 +9,35 @@
  */
 
 
-
-int maxPrimeNumber(int n)
+long long maxPrimeFactors(long long n)
 {
 
-	long long int i, max;
-	
-
-	while (n % 2 == 0)
-	{
-		max = 2;
-		n = n / 2;
-	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-	if (n > 2)
-	{
-		max = n;
-	}
-	return (max);
-
+    long long maxPrime = -1;
+ 
+    while (n % 2 == 0) {
+        maxPrime = 2;
+    }
+    while (n % 3 == 0) {
+        maxPrime = 3;
+        n = n / 3;
+    }
+ 
+    for (int i = 5; i <= sqrt(n); i += 6) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+        while (n % (i + 2) == 0) {
+            maxPrime = i + 2;
+            n = n / (i + 2);
+        }
+    }
+ 
+    if (n > 4)
+        maxPrime = n;
+ 
+    return maxPrime;
 }
-
 /**
  * main - a program that prints the max prime num of a long int.
  *
@@ -48,6 +49,6 @@ void main (void)
 	long long int n;
 
 	n = 612852475143;
-	printf("%lld\n", maxPrimeNumber(n));
+	printf("%lld\n", maxPrimeFactors(n));
 
 }
