09f626dba193c69da0d8a4f3b91ef23b706c22da
akramjerbi91 <akram239911@gmail.com>
2022-02-14 13:31:51 +0100

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the lenght of a string
+ * @s : string
+ *
+ * Return: i the lenght of a string
+ */
+
+int _strlen(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+{
+	i = i;
+}
+	return (i);
+}
