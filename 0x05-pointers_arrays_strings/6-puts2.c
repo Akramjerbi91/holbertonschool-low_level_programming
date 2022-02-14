ee2797591f7131a44b457a0a153dc7b68108a77e
akramjerbi91 <akram239911@gmail.com>
2022-02-14 21:08:04 +0100

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,35 @@
  * Return: string
  */
 
+
+int _strlen(char *str);
+
 void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; i < _strlen(str); i += 2)
 {
 	_putchar(str[i]);
-	i = i + 2;
 }
+
 	_putchar(10);
 
+}
+
+/**
+ * _strlen - counts the lenght of the string
+ * @str : string
+ *
+ * Return: i
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	;
+
+	return (i);
 }
