6de2701cf1469d0506fb562cb476c701b219ff15
akramjerbi91 <akram239911@gmail.com>
2022-02-15 10:33:46 +0100

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,13 +19,13 @@ void puts_half(char *str)
 	/*printf("%d", length);*/
 	if (length % 2 == 0)
 
-	for (i = length / 2; i <= length; i++)
+	for (i = length / 2; i < length; i++)
 
 	_putchar(str[i]);
 
 	else if (length % 2 != 0)
 
-	for (i = (length - 1) / 2; i <= length; i++)
+	for (i = (length + 1) / 2; i < length; i++)
 
 	_putchar(str[i]);
 
