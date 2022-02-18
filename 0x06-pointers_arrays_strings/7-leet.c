46d91594923eefd9553a39c10f40c81b1dab076c
akramjerbi91 <akram239911@gmail.com>
2022-02-18 22:37:18 +0100

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *leet - changes aeotl by 43071
+ *@a: string
+ *
+ *Return: a
+ */
+
+
+char *leet(char *a)
+{
+	int i, j;
+	char l[] = "aAeEoOtTlL";
+	char key[] = "4433007711";
+
+	for (i = 0; i != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (a[i] == l[j])
+
+				a[i] = key[j];
+		}
+	}
+	return (a);
+
+}
