bf57fba0277f450de28ca8da9a28db1db9e7aa4b
akramjerbi91 <akram239911@gmail.com>
2022-02-21 19:36:19 +0100

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ *_strcspn - n of char at beginning 1st str that do not occur in the second str
+ *@s: char
+ *@accept: char
+ *
+ *Return: len
+ */
+
+unsigned int _strcspn(char *s, char *accept);
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: char
+ * @accept: char
+ *
+ * Return: s
+ */
+
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	s += _strcspn(s, accept);
+
+	if ((char *)s)
+		return (s);
+	else
+		return (0);
+}
+
+
+unsigned int _strcspn(char *s, char *accept)
+{
+	unsigned int len = 0, i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (s[i] == accept[j])
+			break;
+		len++;
+	}
+
+	return (len);
+}
