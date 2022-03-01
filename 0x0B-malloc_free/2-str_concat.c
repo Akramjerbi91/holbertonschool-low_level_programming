888ebad3cf99c8784fc6ee3f3053996674372ca5
akramjerbi91 <akram239911@gmail.com>
2022-03-01 18:03:23 +0100

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	char *rtn;
 	int p, q = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	return (NULL);
 
 	rtn = malloc(strlen(s1) + strlen(s2) + 1);
