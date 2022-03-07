aa185e6baf867c06acc6b249fb9291eaa9bc0ba6
akramjerbi91 <akram239911@gmail.com>
2022-03-07 15:49:24 +0100

    fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,25 +11,28 @@
 void print_dog(struct dog *d)
 {
 
-	if (d != NULL)
+	if (d != 0)
 	{
-		if (d->name == NULL)
-			printf("Name:(nill)\nAge:%.1f\nOwner: %s\n", d->age, d->owner);
+		if (d->name == 0 && d->age == 0 && d->owner == 0)
+			printf("Name: (nill)\nAge: (nill)\nOwner: (nill)\n");
 
-		else if (d->age == 0 && d->owner == NULL)
-			printf("Name: %s\nAge:(nill)\nOwner:(nill)\n", d->name);
+		else if (d->age == 0 && d->owner == 0)
+			printf("Name: %s\nAge: (nill)\nOwner: (nill)\n", d->name);
 
-		else if (d->name == NULL && d->age == 0)
-			printf("Name:(nill)\nAge:(nill)\nOwner:%s\n", d->owner);
+		else if (d->name == 0 && d->age == 0)
+			printf("Name: (nill)\nAge: (nill)\nOwner: %s\n", d->owner);
 
-		else if (d->owner == NULL)
-			printf("Name:%s\nAge:%1.f\nOwner:(nill)\n", d->name, d->age);
+		else if (d->name == 0 && d->owner == 0)
+			printf("Name: (nill)\nAge: %1.f\nOwner: (nill)\n", d->age);
+
 
 		else if (d->age == 0)
-			printf("Name:%s\nAge:(nill)\nOwner:%s\n", d->name, d->owner);
+			printf("Name: %s\nAge: (nill)\nOwner: %s\n", d->name, d->owner);
 
-		else if (d->name == NULL && d->owner == NULL)
-			printf("Name:(nill)\nAge:%1.f\nOwner:(nill)\n", d->age);
+		else if (d->owner == 0)
+			printf("Name: %s\nAge: %1.f\nOwner: (nill)\n", d->name, d->age);
+		else if (d->name == 0)
+			printf("Name: (nill)\nAge: %.1f\nOwner: %s\n", d->age, d->owner);
 		else
 			printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
