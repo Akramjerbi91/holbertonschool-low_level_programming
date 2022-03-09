e552e16ec5fbd95485324218d7f7c05f794fd41a
akramjerbi91 <akram239911@gmail.com>
2022-03-09 18:30:15 +0100

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of a given ineger
+ * @array: int
+ * @size: int
+ * @cmp: int function
+ *
+ * Return: index of matched int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]) != 0)
+	{
+		return (i);
+	}
+	}
+	return (-1);
+}
