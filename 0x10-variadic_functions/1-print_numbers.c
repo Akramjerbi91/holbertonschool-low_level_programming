35a7f8e4e5363138b6ef1cfc3a4aa902d348b7f2
akramjerbi91 <akram239911@gmail.com>
2022-03-10 13:52:45 +0100

    tast1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL && i != n - 1)
 		printf("%d%s", va_arg(ap, unsigned int), separator);
