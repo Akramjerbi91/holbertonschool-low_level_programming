2cf91e4e4f49bfef3ff99e5207a5337bc58c3473
Aleph235 <akram239911@gmail.com>
2022-03-24 21:52:43 +0100

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *set_bit - sets a bit at given index
+ * @n: pointer to n
+ * @index: index
+ *
+ * Return: set bit
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask = 1 << index;
+	unsigned int result = *n |= mask;
+
+	if (index > 64)
+		return (-1);
+	if (result == 1)
+		return (1);
+	else
+		return (-1);
+
+}
