6f645dd2fc5fe8622d290b7e7044b29e77ff4dfe
Aleph235 <akram239911@gmail.com>
2022-03-24 19:10:54 +0100

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void invert(unsigned long int n)
 	if (n == 0)
 		return;
 
-invert(n >> 1);
+	invert(n >> 1);
 
 	if ((n & 1) == 1)
 		_putchar('1');
@@ -30,5 +30,5 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-invert(n);
+	invert(n);
 }
