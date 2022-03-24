519af036fc50465ddf9ad28cc115bfe17eac23aa
Aleph235 <akram239911@gmail.com>
2022-03-24 19:06:47 +0100

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * invert -  1's and 0's
+ * @n: insigned long int
+ * Return: void
+ */
+void invert(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+invert(n >> 1);
+
+	if ((n & 1) == 1)
+		_putchar('1');
+
+	if ((n & 1) == 0)
+		_putchar('0');
+}
+/**
+ * print_binary - prints binary from decimal
+ * @n: unsigned long int input
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+invert(n);
+}
