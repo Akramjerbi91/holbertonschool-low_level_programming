d98797be2ff4c03a7ac06afdd9aa028d945648f2
Aleph235 <akram239911@gmail.com>
2022-03-24 22:05:48 +0100

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,10 +13,12 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask = 1 << index;
 	int result;
 
-	result = *n |= mask;
 
 	if (index > 64)
 		return (-1);
+
+	result = *n |= mask;
+
 	if (result)
 		return (1);
 	else
