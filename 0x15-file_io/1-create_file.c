04e7e6e3b1e6d704aeb4a88be98a8384f7f070f4
Aleph235 <akram239911@gmail.com>
2022-03-28 19:34:30 +0200

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, sz;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,9 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		write(fd, NULL, 1);
-	else
-		write(fd, text_content, strlen(text_content));
+		return (-1);
+	sz = write(fd, text_content, strlen(text_content));
 	close(fd);
-	return (1);
+	return (sz);
 }
