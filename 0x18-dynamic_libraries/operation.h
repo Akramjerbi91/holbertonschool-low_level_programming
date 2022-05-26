c1209d86e772e831400df276355a2679e4b07ceb
Aleph235 <akram239911@gmail.com>
2022-05-26 16:31:54 +0200

    creating hashtable with c

---
+++ 0x18-dynamic_libraries/operation.h
@@ -0,0 +1,11 @@
+#ifndef operation 
+#define operation 
+/*operation functions*/
+extern int op_add(int a, int b);
+extern int op_sub(int a, int b);
+extern int op_mul(int a, int b);
+extern int op_div(int a, int b);
+extern int op_mod(int a, int b);
+
+
+#endif
